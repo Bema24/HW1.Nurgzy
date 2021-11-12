05703917be2ebf7182955761050d6391853bdab7
Bema24 <bema.baktybekova03@gmail.com>
2021-11-12 18:49:54 +0300

    Initial commit

---
+++ src/com/company/LimitException.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class LimitException extends  Exception {
+    public LimitException(String message, double remainingAmount) {
+        super(message);
+
+    }
+
+        public static void getRemainingAmount(){
+
+
+        }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Ввидите пароль:");
+            int password = scanner.nextInt();
+
+            if (password == 242836) {
+                System.out.println(password);
+                BankAccount user = new BankAccount();
+                System.out.println("Hello Ms.Bermet Baktybekova");
+
+                System.out.println("You have 20.000");
+                user.deposit(20000);
+                System.out.println("Снимите желаемую сумму:");
+                int money = scanner.nextInt();
+                System.out.println(user.getAmount());
+                while (true) {
+                    try {
+                        user.withDraw (money);
+                    } catch (LimitException a) {
+                        try {
+                            user.withDraw((int) user.getAmount());
+                        } catch (LimitException i) {
+                            i.printStackTrace();
+                        }
+                        break;
+                    }
+
+
+                }
+            }else{
+                System.out.println("Пароль не верный");
+            }
+
+    }
+}
